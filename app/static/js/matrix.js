5592177f0b9f28a4ebd3e37abf294a965c3c9a68
Nestor <148357514+nPreissler@users.noreply.github.com>
2025-01-12 17:58:37 -0300

    style: calculate button are disabled when page load

---
+++ app/static/js/matrix.js
@@ -0,0 +1,18 @@
+// get rows 
+//get cols
+// -- user click
+// create a perimeter for matrix
+// create one input element for row and column
+// __ return it to page
+// -- user fill fields
+// -- user click (calculate)
+//  get the fields numbers 
+// send to backend in a compatible format
+// ******backend-precessing*****
+// backend brings reuslts back
+// __return it to page
+
+const calculateButton = document.getElementById('calculate')
+calculateButton.style.background = '#ccc' 
+calculateButton.disabled = true
+//after page is loaded, button turns at disabled status
