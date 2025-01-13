dbfbf9f42760bb2e50ad79fdcb06640a377c72e9
Nestor <148357514+nPreissler@users.noreply.github.com>
2025-01-13 16:47:27 -0300

    refactor: calculate def varuables are changed to perform user data

--- app/routes.py
+++ app/routes.py
@@ -10,21 +10,26 @@ def index():
 
 @main.route('/calculate', methods=['POST'])
 def calculate():
-    data = request.json
-    matrix_a = data.get('matrix_a')
-    matrix_b = data.get('matrix_b')
-    operation = data.get('operation')
+    data = request.get_json()
+    rows = data['rows']
+    cols = data['cols']
+    matrix_a = data['matrixA']
+    matrix_b = data['matrixB']
+    operation = data['operation']
+
+    matrix_a_2d = [matrix_a[i * cols:(i + 1) * cols] for i in range(rows)]
+    matrix_b_2d = [matrix_b[i * cols:(i + 1) * cols] for i in range(rows)]
 
     try:
         if operation == 'add':
-            result = add_matrices(matrix_a, matrix_b)
-        elif operation == 'subtraction':
-            result = subtract_matrices(matrix_a, matrix_b)
+            result = add_matrices(matrix_a_2d, matrix_b_2d)
+        elif operation == 'subtract':
+            result = subtract_matrices(matrix_a_2d, matrix_b_2d)
         elif operation == 'multiply':
-            result = multiply_matrices(matrix_a, matrix_b)
+            result = multiply_matrices(matrix_a_2d, matrix_b_2d)
         else:
-            return jsonify({'error':'Invalid Operation'}), 400
-        
-        return jsonify({'result': result})
+            return jsonify({'error': 'Invalid operation'}), 400
+
+        return jsonify(result)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
