a9d9f11f4dc39257f044c57f27790db344ccc944
Nestor <148357514+nPreissler@users.noreply.github.com>
2025-01-15 22:11:08 -0300

    fix(deploy): error response from render --> ImportError: cannot import name 'app' from 'app'

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,11 @@
 from flask import Flask 
 
 def create_app():
-    app = Flask(__name__)
+    appl = Flask(__name__)
 
 
     # rotes register 
     from .routes import main
-    app.register_blueprint(main)
+    appl.register_blueprint(main)
 
-    return app
+    return appl
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from app import app
+from app import appl
 
 if __name__ == "__main__":
-    app.run()
+    appl.run()
